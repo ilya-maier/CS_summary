03e0d235bcd7de9764bb6a8afb5ae28c55c9aecc
Ilya Maier <ilya.maier@outlook.com>
2020-04-12 23:40:15 +0200

    Added insertion sort

---
+++ algorithms/sorting/insertion_sort.py
@@ -0,0 +1,32 @@
+"""
+    Insertion Sort:
+        inserts each element into already sorted array until done
+    Time Complexity: O(n^2) -> worst
+    Space Complexity: O(1)
+
+    https://en.wikipedia.org/wiki/Insertion_sort
+"""
+import time
+from random import random
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j] < arr[j - 1] and j > 0:
+            temp = arr[j - 1]
+            arr[j - 1] = arr[j]
+            arr[j] = temp
+            j -= 1
+
+
+print("Normal version:")
+arr = [int(random() * 100) for _ in range(100)]
+print(arr)
+
+start = time.time()
+insertion_sort(arr)
+end = time.time()
+
+print(arr)
+print(f"Time elapsed: {end - start}\n")
