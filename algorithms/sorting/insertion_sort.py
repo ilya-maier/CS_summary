2a82fde9db1595ed0a98cd158361f74bf0df646a
Ilya Maier <ilya.maier@outlook.com>
2020-04-16 22:50:22 +0200

    Remove insertion sort optimization

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -6,8 +6,6 @@
 
     https://en.wikipedia.org/wiki/Insertion_sort
 """
-from bisect import bisect_left
-
 from helpers import test_sorting_algorithm
 
 
@@ -21,17 +19,4 @@ def insertion_sort(arr):
             j -= 1
 
 
-# Time Complexity: O(n*log(n)) -> worst
-# Space Complexity: O(n)
-def insertion_sort_optimized(arr):
-    array = [arr[0]]
-    for i in range(1, len(arr)):
-        index = bisect_left(array, i)
-        array.insert(index, i)
-    return array
-
-
 test_sorting_algorithm(insertion_sort)
-test_sorting_algorithm(insertion_sort_optimized,
-                       version="optimized",
-                       returns=True)
