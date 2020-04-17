eca5f02981228285d3924b986c0b771b23765842
Ilya Maier <ilya.maier@outlook.com>
2020-04-17 18:33:57 +0200

    Added quicksort in place

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -8,6 +8,7 @@
     Space Complexity: O(log(n))
 
     https://en.wikipedia.org/wiki/Quicksort
+    https://stackoverflow.com/questions/18262306/quicksort-with-python
 """
 from helpers import test_sorting_algorithm
 
@@ -26,4 +27,31 @@ def quicksort(arr):
     return quicksort(smaller_part) + [pivot] + quicksort(larger_part)
 
 
+def partition(arr, begin, end):
+    pivot = begin
+    for i in range(begin + 1, end):
+        if arr[i] <= arr[begin]:
+            pivot += 1
+            arr[i], arr[pivot] = arr[pivot], arr[i]
+
+    # each element between arr[begin] and arr[pivot] is <= than arr[begin],
+    # therefore we need to swap them
+    arr[pivot], arr[begin] = arr[begin], arr[pivot]
+
+    return pivot
+
+
+def quicksort_in_place(arr, begin=0, end=None):
+    if end is None:
+        end = len(arr)
+
+    if begin >= end:
+        return
+
+    pivot = partition(arr, begin, end)
+    quicksort_in_place(arr, begin, pivot)
+    quicksort_in_place(arr, pivot + 1, end)
+
+
 test_sorting_algorithm(quicksort, returns=True)
+test_sorting_algorithm(quicksort_in_place)
