c637f367f3227b6e062407700b179b560a7b7fbe
Ilya Maier <ilya.maier@outlook.com>
2020-04-23 18:31:48 +0200

    Added more features to Hash Table

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -4,6 +4,7 @@ class HashTable:
         self.arr = [None] * size
         self.keys = []
         self.values = []
+        self.length = 0
         self.size = size
         self.add(**kwargs)
 
@@ -13,11 +14,23 @@ class HashTable:
             out += f"{self.keys[k]}: {self.values[k]}, "
         return out[:-2] + "}"
 
+    def __len__(self):
+        return self.length
+
     def add(self, **kwargs):
         for key, value in kwargs.items():
             self.keys.append(key)
             self.values.append(value)
             self.arr[self.custom_hash(key)] = value
+            self.length += 1
+
+    def get(self, key):
+        return self.arr[self.custom_hash(key)]
+
+    def remove(self, key):
+        del self.values[self.keys.index(key)]
+        self.keys.remove(key)
+        self.arr[self.custom_hash(key)] = None
 
     def custom_hash(self, key):
         num = 0
@@ -27,6 +40,13 @@ class HashTable:
 
 
 hash_table = HashTable(key=2, qwe=10, arr=123, first=-1)
+
 print(hash_table)
 print(hash_table.keys)
 print(hash_table.values)
+
+hash_table.add(test="test")
+hash_table.remove("qwe")
+print(hash_table)
+print(hash_table.get("test"))
+print(len(hash_table))
